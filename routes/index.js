e260088a30899a054a7adc337facc6b8aae2631c
Cristiano SB <ti.maby2020@gmail.com>
2023-10-31 18:08:40 -0300

    iniciado em git

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Faculdade Unyleia-ENCURTADOR DE URL' });
+});
+
+function generateCode(){
+
+  let text ='';
+  const possible='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0987654321';
+  for(let i=0; i < 5; i++)
+  text += possible.charAt(Math.floor(Math.random()* possible.length));
+return text;
+}
+
+
+
+router.post ('/new', (req, res, next) => {
+const url=req.body.url;
+console.log(url);
+const code=generateCode();
+
+res.send('http://localhost:3000' + code);
+})
+
+module.exports = router;
